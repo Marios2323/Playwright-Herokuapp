e2a023d215370b09d831ec45b9ee7af495f94f41
Michael <michaelgalliko@gmail.com>
2026-01-28 15:43:18 +0200

    rename test to herokuapp.spec.js

--- test.js
+++
@@ -1,8 +0,0 @@
-const { chromium } = require('playwright');
-
-(async () => {
-  const browser = await chromium.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto('https://example.com');
-  console.log("Browser should be open now");
-})();
---
+++ tests/herokuapp.spec.js
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('login and logout on herokuapp', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/login');
+
+  await page.getByRole('textbox', { name: 'Username' }).fill('tomsmith');
+  await page.getByRole('textbox', { name: 'Password' }).fill('SuperSecretPassword!');
+  await page.getByRole('button', { name: ' Login' }).click();
+  
+  await page.getByRole('link', { name: 'Logout' }).click();
+});
