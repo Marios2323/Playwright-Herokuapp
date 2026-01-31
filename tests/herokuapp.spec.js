d7a52a8d625c891471fa1a6a6d883e4952743073
Michael <michaelgalliko@gmail.com>
2026-01-31 07:56:41 +0200

    update herokuapp.spec.js

--- tests/herokuapp.spec.js
+++ tests/herokuapp.spec.js
@@ -2,35 +2,22 @@ import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
 
 test("Happy path - Login test for herokuapp", async ({ page }) => {
-    // await page.goto('https://the-internet.herokuapp.com/login');
     const loginPage = new LoginPage(page);
     await loginPage.navigate();
-    // await page.getByRole('textbox', { name: 'Username' }).click();
-    // await page.getByRole('textbox', { name: 'Username' }).fill('tomsmith');
-    // await page.getByRole('textbox', { name: 'Password' }).click();
-    // await page.getByRole('textbox', { name: 'Password' }).fill('SuperSecretPassword!');
-    await loginPage.login('tomsmith', 'SuperSecretPassword!')
-    // await page.getByRole('button', { name: ' Login' }).click();
-    await loginPage.clickLogin();
-    // await page.pause();
+    await loginPage.login('tomsmith', 'SuperSecretPassword!');
+    await expect (loginPage.secureMessage).toContainText('Secure Area');
 })
 
 test("Wrong username", async ({ page }) => {
     const loginPage = new LoginPage(page);
     await loginPage.navigate();
-    await loginPage.wrongUsernameLogin();
-    await loginPage.clickLogin();
+    await loginPage.login('wrongUser', 'SuperSecretPassword!');
     await expect(loginPage.flashMessage).toContainText('Your username is invalid!');
-
-    // await page.pause();
 });
 
 test("Wrong password", async ({ page }) => {
     const loginPage = new LoginPage(page);
     await loginPage.navigate();
-    await loginPage.wrongPasswordLogin();
-    await loginPage.clickLogin();
+    await loginPage.login('tomsmith', 'wrong password');
     await expect(loginPage.flashMessage).toContainText('Your password is invalid!');
-
-    // await page.pause();
 });
