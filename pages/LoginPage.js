512cf632d61c062db7dd46cb93023b2f46632c4a
Michael <michaelgalliko@gmail.com>
2026-01-31 07:55:35 +0200

    update LoginPage.js

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,38 +1,22 @@
 export class LoginPage {
     constructor(page) {
         this.page = page;
+
+        //locators
+        this.usernameInput = page.getByRole('textbox', { name: 'Username' });
+        this.passwordInput = page.getByRole('textbox', { name: 'Password' });
+        this.loginButton = page.getByRole('button', { name: 'Login' });
         this.flashMessage = page.locator('#flash');
+        this.secureMessage = page.locator('#content');
     }
+
     async navigate() {
         await this.page.goto('https://the-internet.herokuapp.com/login');
     }
 
     async login(username, password) {
-        await this.page.getByRole('textbox', { name: 'Username' }).click();
-        await this.page.getByRole('textbox', { name: 'Username' }).fill('tomsmith');
-        await this.page.getByRole('textbox', { name: 'Password' }).click();
-        await this.page.getByRole('textbox', { name: 'Password' })
-            .fill('SuperSecretPassword!');
-    }
-
-    async clickLogin() {
-        await this.page.getByRole('button', { name: ' Login' }).click();
-    }
-
-    async wrongUsernameLogin() {
-        await this.page.getByRole('textbox', { name: 'Username' }).click();
-        await this.page.getByRole('textbox', { name: 'Username' }).fill('wrong username');
-        await this.page.getByRole('textbox', { name: 'Password' }).click();
-        await this.page.getByRole('textbox', { name: 'Password' })
-            .fill('SuperSecretPassword!');
-        // await expect(this.page.locator(id="flash")).toContainText('Your username is invalid!');
-    }
-
-    async wrongPasswordLogin() {
-        await this.page.getByRole('textbox', { name: 'Username' }).click();
-        await this.page.getByRole('textbox', { name: 'Username' }).fill('tomsmith');
-        await this.page.getByRole('textbox', { name: 'Password' }).click();
-        await this.page.getByRole('textbox', { name: 'Password' })
-            .fill('wrong password');
+        await this.usernameInput.fill(username);
+        await this.passwordInput.fill(password);
+        await this.loginButton.click();
     }
 }
