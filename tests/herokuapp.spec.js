f34d0f0fa80b4290913624320bd16ec5af4eff89
Michael <michaelgalliko@gmail.com>
2026-02-22 08:44:05 +0200

    feat: add login tests using POM

--- tests/herokuapp.spec.js
+++ tests/herokuapp.spec.js
@@ -1,23 +1,51 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
 
-test("Happy path - Login test for herokuapp", async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.navigate();
-    await loginPage.login('tomsmith', 'SuperSecretPassword!');
-    await expect (loginPage.secureMessage).toContainText('Secure Area');
-})
+const testData = [
+    {
+        name: 'Happy path',
+        username: 'tomsmith',
+        password: 'SuperSecretPassword!',
+        expectedMessage: 'Secure Area',
+        success: true
+    },
+    {
+        name: 'Wrong username',
+        username: 'wrongUser',
+        password: 'SuperSecretPassword!',
+        expectedMessage: 'Your username is invalid!',
+        success: false
+    },
+    {
+        name: 'Wrong password',
+        username: 'tomsmith',
+        password: 'wrong password',
+        expectedMessage: 'Your password is invalid!',
+        success: false
+    }
+];
 
-test("Wrong username", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
     const loginPage = new LoginPage(page);
     await loginPage.navigate();
-    await loginPage.login('wrongUser', 'SuperSecretPassword!');
-    await expect(loginPage.flashMessage).toContainText('Your username is invalid!');
 });
 
-test("Wrong password", async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.navigate();
-    await loginPage.login('tomsmith', 'wrong password');
-    await expect(loginPage.flashMessage).toContainText('Your password is invalid!');
+test.describe('Login Tests', () => {
+
+    for (const data of testData) {
+
+        test(data.name, async ({ page }) => {
+            const loginPage = new LoginPage(page);
+            await loginPage.navigate();
+            await loginPage.login(data.username, data.password);
+
+            if (data.success) {
+                await expect(loginPage.secureMessage)
+                    .toContainText(data.expectedMessage);
+            } else {
+                await expect(loginPage.flashMessage)
+                    .toContainText(data.expectedMessage);
+            }
+        });
+    }
 });
